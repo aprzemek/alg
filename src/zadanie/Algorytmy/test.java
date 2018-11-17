aee76f7953075f5340d93f73f7f4f7c6b7434030
PRZEMEK <a.przmeke@o2.pl>
2018-11-17 10:39:08 +0100

    Algorymty

---
+++ src/zadanie/Algorytmy/test.java
@@ -0,0 +1,24 @@
+package zadanie.Algorytmy;
+
+import java.util.Scanner;
+
+public class test {
+
+
+          static int sum(int a, int b){
+            return a+b;
+}
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a=2;
+        a = in.nextInt();
+        int b =3;
+        b = in.nextInt();
+        int sum = 0;
+
+        System.out.println(sum);
+    }
+}
+
